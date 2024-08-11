c5328cab88fbc3ff969d10d403b6508a67b76b97
stasyabunina <118092308+stasyabunina@users.noreply.github.com>
2024-08-11 04:58:14 +0300

    update validator

---
+++ src/js/components/formatNumber.js
@@ -0,0 +1,4 @@
+export const formatNumber = (number) => number.split("").reduce((seed, next, index) => {
+  if (index !== 0 && !(index % 4)) seed += " ";
+  return seed + next;
+}, "");
--- src/js/components/onload.js
+++ src/js/components/onload.js
@@ -1,11 +1,16 @@
 import ValidationForm from "./validator";
 import { input, form } from "./var";
+import { formatNumber } from "./formatNumber";
 
 const validateForm = new ValidationForm(form);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const value = input.value;
-  validateForm.init(value);
   input.focus();
+  const value = input.value.replaceAll(' ', '');
+  validateForm.init(value);
+});
+
+input.addEventListener("input", () => {
+  input.value = formatNumber(input.value.replaceAll(" ", ""))
 });
